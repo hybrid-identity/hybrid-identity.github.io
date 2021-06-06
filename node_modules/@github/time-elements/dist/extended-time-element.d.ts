36329192f2792814e10eeabc6d3f30e4a979c333
Nicholas Frota <info@nicholasfrota.com>
2021-06-05 21:22:21 -0300

    test

--- node_modules/@github/time-elements/dist/extended-time-element.d.ts
+++
@@ -1,8 +0,0 @@
-export default class ExtendedTimeElement extends HTMLElement {
-    static get observedAttributes(): string[];
-    connectedCallback(): void;
-    attributeChangedCallback(attrName: string, oldValue: string, newValue: string): void;
-    get date(): Date | null;
-    getFormattedTitle(): string | undefined;
-    getFormattedDate(): string | undefined;
-}
